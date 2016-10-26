bae9bfb4d76b9a7f40ff858ef7d6e830b522658b
Dan <danzelcode@gmail.com>
2016-10-26 17:40:34 +0300

    lesson 64

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {RecipeService} from "./recipes/recipe.service";
+import {ShoppingListService} from "./shoping-list/shopping-list.service";
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [RecipeService, ShoppingListService],
+
 })
 export class AppComponent {
-  title = 'rb works!';
+
 }
